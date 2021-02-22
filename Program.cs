0e90e6787efd10e14cff23b64fed96d4e1db6e2e
Yaren <avote841@gmail.com>
2021-02-22 14:37:50 +0300

    Linq and Join-DTO

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId = 1 , CategoryName = "PC"},
+                new Category{CategoryId = 2, CategoryName = "Telefon"}
+            };
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId = 1, CategoryId=1, ProductName = "ASUS Laptop", QuantityPerUnit ="32 gb ram", UnitPrice = 10000,UnitsInStock= 5 },
+                new Product{ProductId = 2, CategoryId=1, ProductName = "HP Laptop", QuantityPerUnit ="16 gb ram", UnitPrice = 8000,UnitsInStock= 3},
+                new Product{ProductId = 3, CategoryId=1, ProductName = "Acer Laptop", QuantityPerUnit ="8 gb ram", UnitPrice = 6000,UnitsInStock= 2 },
+                new Product{ProductId = 4, CategoryId=2, ProductName = "Samsung Telefon", QuantityPerUnit ="4 gb ram", UnitPrice = 5000,UnitsInStock= 15 },
+                new Product{ProductId = 5, CategoryId=2, ProductName = "Apple Telefon", QuantityPerUnit ="4 gb ram", UnitPrice = 8000,UnitsInStock= 0 }
+            };
+
+            //    Console.WriteLine("---------------Algoritmik--------------");
+            //    foreach (var product in products)
+            //    {
+            //        if (product.UnitPrice > 5000 && product.UnitsInStock>3)
+            //        {
+            //            Console.WriteLine(product.ProductName);
+            //        }
+
+            //    }
+            //    Console.WriteLine("-------------LINQ---------------");
+            //    var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);
+            //    foreach (var product in result)
+            //    {
+            //        Console.WriteLine(product.ProductName);
+            //    }
+            //    GetProducts(products);
+
+            //}
+
+            ////LINQ yoksa;
+            //static List<Product> GetProducts(List<Product> products)
+            //{
+            //    List<Product> filteredProducts = new List<Product>();
+            //    foreach (var product in products)
+            //    {
+            //        if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+            //        {
+            //            filteredProducts.Add(product);
+            //        }
+
+            //    }
+            //    return filteredProducts;
+
+            //}
+            //static List<Product> GetProductsLinq(List<Product> products)
+            //{
+            //    return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3).ToList();
+            //}
+
+            //var result= products.Any(p=> p.ProductName == "Acer Laptop");
+            //  Console.WriteLine(result);
+
+            //var result =  products.Find(p => p.ProductId == 3);
+            // Console.WriteLine(result.ProductName); 
+
+            //var result =   products.FindAll(p => p.ProductName.Contains("top"));
+            //  Console.WriteLine(result); -- liste döndü
+
+
+            //Single Line Query
+            //var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            //foreach (var product in result)
+            //{
+            //    Console.WriteLine(product.ProductName);
+
+            //}
+
+            //var result = from p in products
+            //             where p.UnitPrice > 6000
+            //             orderby p.UnitPrice descending, p.ProductName ascending
+            //             select new ProductDTO { ProductId = p.ProductId, ProductName = p.ProductName , UnitPrice = p.UnitPrice};
+            //foreach (var p in result)
+            //{
+            //    Console.WriteLine(p.ProductName);
+            //}
+
+
+            //JOIN ETME
+
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice > 5000
+                         select new ProductDTO { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            foreach (var productDTO in result)
+            {
+                Console.WriteLine("{0} ------ {1}", productDTO.ProductName,  productDTO.CategoryName);
+            }
+
+        }
+        class ProductDTO
+        {
+            public string CategoryName { get; set; }
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public decimal UnitPrice { get; set; }
+        }  
+        
+
+        class Product
+        {
+            public int ProductId { get; set; }
+            public int CategoryId { get; set; }
+
+            public string ProductName { get; set; }
+            public string QuantityPerUnit { get; set; }
+            public decimal UnitPrice { get; set; }
+            public int UnitsInStock { get; set; }
+        }
+
+        class Category
+        {
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+        }
+    }
+}
